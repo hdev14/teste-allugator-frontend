8e81cd47d7b639f642fdee263df6a097c111b4cd
Hermerson Araújo <ne.hermerson@gmail.com>
2020-11-14 10:21:21 -0300

    feat: finish SearchBySalary form

--- src/components/SearchBySalary/index.tsx
+++ src/components/SearchBySalary/index.tsx
@@ -1,29 +1,56 @@
 import React, { useState } from 'react';
+import httpClient from '../../http-client';
+import { EmployeeData } from '../../types';
 import Button from '../Button';
 
-const SearchBySalary: React.FC = () => {
-  const [min, setMin] = useState(0);
-  const [max, setMax] = useState(0);
+interface SearchBySalaryProps {
+  setEmployees: (employees: EmployeeData[]) => void
+}
 
-  const submitHandler = () => {};
+const SearchBySalary: React.FC<SearchBySalaryProps> = ({ setEmployees }) => {
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(1000);
+
+  const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (max < 0) return;
+
+    try {
+      const response = await httpClient.get<EmployeeData[]>(`/employees/salary?min=${min}&max=${max}`);
+      setEmployees(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <form onSubmit={submitHandler}>
-      <input
-        type="number"
-        name="min"
-        placeholder="Digite o valor minímo"
-        value={min}
-        onChange={(e) => setMin(parseFloat(e.target.value))}
-      />
+      <label htmlFor="min">
+        Valor miníno
+        <input
+          required
+          type="number"
+          name="min"
+          placeholder="Digite o valor minímo"
+          value={min}
+          onChange={(e) => setMin(parseFloat(e.target.value))}
+          min={0}
+        />
+      </label>
 
-      <input
-        type="number"
-        name="max"
-        placeholder="Digite o valor máximo"
-        value={max}
-        onChange={(e) => setMax(parseFloat(e.target.value))}
-      />
+      <label htmlFor="max">
+        Valor máximo
+        <input
+          required
+          type="number"
+          name="max"
+          placeholder="Digite o valor máximo"
+          value={max}
+          onChange={(e) => setMax(parseFloat(e.target.value))}
+          min={1000}
+        />
+      </label>
 
       <Button type="submit" btnType="secundary"> pesquisar </Button>
     </form>
