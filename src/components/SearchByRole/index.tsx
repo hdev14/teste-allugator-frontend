655212c816dd77686cf0d52183d2a3d7992a9bb0
Hermerson Araújo <ne.hermerson@gmail.com>
2020-11-14 09:55:57 -0300

    feat: finish SearchByRole form

--- src/components/SearchByRole/index.tsx
+++ src/components/SearchByRole/index.tsx
@@ -1,17 +1,34 @@
 import React, { useState } from 'react';
+import httpClient from '../../http-client';
+import { EmployeeData } from '../../types';
 import Button from '../Button';
 
-const SearchByRole: React.FC = () => {
+interface SearchByRoleProps {
+  setEmployees: (employees: EmployeeData[]) => void
+}
+
+const SearchByRole: React.FC<SearchByRoleProps> = ({ setEmployees }) => {
   const [role, setRole] = useState('');
 
-  const submitHandler = () => {};
+  const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!role) return;
+
+    try {
+      const response = await httpClient.get<EmployeeData[]>(`/employees/role?role=${role}`);
+      setEmployees(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <form onSubmit={submitHandler}>
       <input
         type="text"
-        name="cpf"
-        placeholder="Digite o CPF do funcionário"
+        name="role"
+        placeholder="Digite o cargo do funcionário"
         value={role}
         onChange={(e) => setRole(e.target.value)}
       />
