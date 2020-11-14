e807267dc1429a85732c4f617b52275ab60c64b8
Hermerson Araújo <ne.hermerson@gmail.com>
2020-11-14 10:01:10 -0300

    feat: finish SearchByCPF form

--- src/components/SearchByCPF/index.tsx
+++ src/components/SearchByCPF/index.tsx
@@ -1,10 +1,28 @@
 import React, { useState } from 'react';
+import httpClient from '../../http-client';
+
+import { EmployeeData } from '../../types';
 import Button from '../Button';
 
-const SearchByCPF: React.FC = () => {
+interface SearchByCPFProps {
+  setEmployees: (employees: EmployeeData[]) => void
+}
+
+const SearchByCPF: React.FC<SearchByCPFProps> = ({ setEmployees }) => {
   const [cpf, setCpf] = useState('');
 
-  const submitHandler = () => {};
+  const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!cpf) return;
+
+    try {
+      const response = await httpClient.get<EmployeeData>(`/employees/cpf?cpf=${cpf}`);
+      setEmployees([response.data]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <form onSubmit={submitHandler}>
