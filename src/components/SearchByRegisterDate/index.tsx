805e36d7f402f08be25f42cce60363279a0dd364
Hermerson Araújo <ne.hermerson@gmail.com>
2020-11-14 09:50:02 -0300

    feat: finish SearchByRegisterDate form

--- src/components/SearchByRegisterDate/index.tsx
+++ src/components/SearchByRegisterDate/index.tsx
@@ -1,17 +1,36 @@
 import React, { useState } from 'react';
+import InputMask from 'react-input-mask';
+import httpClient from '../../http-client';
+
+import { EmployeeData } from '../../types';
 import Button from '../Button';
 
-const SearchByRegisterDate = () => {
+interface SearchByRegisterDateProps {
+  setEmployees: (employees: EmployeeData[]) => void
+}
+
+const SearchByRegisterDate: React.FC<SearchByRegisterDateProps> = ({ setEmployees }) => {
   const [date, setDate] = useState('');
 
-  const submitHandler = () => {};
+  const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!date) return;
+
+    try {
+      const response = await httpClient.get<EmployeeData[]>(`/employees/register-date?date=${date}`);
+      setEmployees(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <form onSubmit={submitHandler}>
-      <input
-        type="number"
-        name="uf"
-        placeholder="Digite o status"
+      <InputMask
+        type="text"
+        name="register-date"
+        placeholder="DD/MM/AAAA"
+        mask="99/99/9999"
         value={date}
         onChange={(e) => setDate(e.target.value)}
       />
